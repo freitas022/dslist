2cb88955c280d00f7eec0b73d4cd3f1a9b73a251
Matheus <mf022rj@gmail.com>
2023-07-10 21:32:59 -0300

    Initial commit

---
+++ src/main/java/com/example/gamecollection/GameCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamecollection/GameCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

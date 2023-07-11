132b5347cbc2577b036b67a4ecf24d879aae0e45
Matheus <mf022rj@gmail.com>
2023-07-10 21:44:00 -0300

    properties, Game Entity, ORM

---
+++ src/main/java/com/example/gamecollection/entities/Game.java
@@ -0,0 +1,31 @@
+package com.example.gamecollection.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private int year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
